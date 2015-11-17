7c20d77bc70eb669f3c0f965115927dd6d654c12
TheFry <Luke@pcp079820pcs.wireless.calpoly.edu>
2015-11-17 14:43:55 -0800

    Added tab view template, initial template

---
+++ Playlist Dispenser/Playlist Dispenser/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Playlist Dispenser
+//
+//  Created by Luke Matusiak on 11/17/15.
+//  Copyright © 2015 Luke Matusiak. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Playlist Dispenser/Playlist Dispenser/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Playlist Dispenser
+//
+//  Created by Luke Matusiak on 11/17/15.
+//  Copyright © 2015 Luke Matusiak. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
